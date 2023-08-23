9d9658db3681d0257e4fd76ba6b1fc37c8a221d1
aliciaguiradelo <alicia.guiradelo@outlook.com>
2023-08-22 22:07:50 -0300

    adicionando url favoritos

--- saga/src/main/java/br/com/br/saga/controllers/FavoritoController.java
+++ saga/src/main/java/br/com/br/saga/controllers/FavoritoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FavoritoController {
 
-    @GetMapping()
+    @GetMapping("/favoritos")
     public Favorito listAll(){
         return new Favorito(1L,1L);
     }
