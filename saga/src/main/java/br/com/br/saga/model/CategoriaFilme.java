01aa55b3d725d9506c1ef4503d950259294592c6
unknown <alicia.guiradelo@outlook.com>
2023-10-28 12:02:26 -0300

    implementa paginação

--- saga/src/main/java/br/com/br/saga/model/CategoriaFilme.java
+++ saga/src/main/java/br/com/br/saga/model/CategoriaFilme.java
@@ -1,9 +1,5 @@
 package br.com.br.saga.model;
 
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
